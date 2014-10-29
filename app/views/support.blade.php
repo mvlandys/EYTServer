2749e2b72221de7f6fa59be9b3373556ab083b76
Mathew Vlandys <mvlandys@gmail.com>
2014-10-29 02:47:03 +0000

    Updated Support Page

--- app/views/support.blade.php
+++ app/views/support.blade.php
@@ -1,7 +1,8 @@
 @extends('layout')
 
 @section('content')
-
-For support please email: stevenh@uow.edu.au
+<br/>
+For support please email: stevenh@uow.edu.au<br/>
+For server install & documentation please visit: <a href="https://github.com/mvlandys/EYTServer">https://github.com/mvlandys/EYTServer</a>
 
 @stop
